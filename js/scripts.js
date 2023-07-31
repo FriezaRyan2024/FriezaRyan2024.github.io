058d7e443ba591621b81257f6487387086ef219e
Ryan Brooks <r.brooks@trengo.com>
2023-07-31 17:44:07 +0200

    formatting

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
     $('.wp1').waypoint(function () {
         $('.wp1').addClass('animated fadeInLeft');
     }, {
-        offset: '75%'
+        // offset: '75%'
     });
     $('.wp2').waypoint(function () {
         $('.wp2').addClass('animated fadeInRight');
     }, {
-        offset: '75%'
+        // offset: '75%'
     });
     $('.wp3').waypoint(function () {
         $('.wp3').addClass('animated fadeInLeft');
