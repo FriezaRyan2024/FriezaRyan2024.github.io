968fa7a9e6ac351ba0dc1d01f7302415541c8497
Ryan Brooks <r.brooks@trengo.com>
2023-09-10 20:43:09 +0200

    fixes 2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -132,8 +132,7 @@ $(document).ready(function () {
       } else {
         $("section.navigation").removeClass("fixed");
         $("header").css({
-          "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-          padding: "30px 0", // TODO: HEADER HEIGHT
+          padding: "30px 0",
         });
         $("header .member-actions").css({
           top: "41px",
