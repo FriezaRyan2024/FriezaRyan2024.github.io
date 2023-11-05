fc6a5269d333e3b321f17a8363bccf170b5aecad
Ryan Brooks <r.brooks@trengo.com>
2023-11-05 14:25:55 +0100

    Fixed calendar RSVP date

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -203,14 +203,14 @@ $(document).ready(function () {
       title: "Frieza and Ryan's Wedding",
 
       // Event start date
-      start: new Date("March 1, 2023 15:00"),
+      start: new Date("March 1, 2024 18:30"),
 
       // Event duration (IN MINUTES)
       // duration: 120,
 
       // You can also choose to set an end time
       // If an end time is set, this will take precedence over duration
-      end: new Date("Nov 29, 2017 00:00"),
+      end: new Date("March 1, 2024 00:00"),
 
       // Event Address
       address: "Westervilla, Amsterdam",
